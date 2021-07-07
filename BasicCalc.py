389021e37f423b4227d2b6bdb2f112ffc3a53c0a
Harshal-14git <87035356+Harshal-14git@users.noreply.github.com>
2021-07-08 00:29:47 +0530

    Day 1 Assignment submitted

---
+++ BasicCalc.py
@@ -0,0 +1,11 @@
+# Assignment Day 1 Addition Substraction Multiplication Division
+a = int(input("Enter first number="))
+b = int(input("Enter second number="))
+sum=a+b
+sub=a-b
+mul=a*b
+div=a/b
+print("Addition=",sum)
+print("Substraction=",sub)
+print("Multiplication=",mul)
+print("Division=",div)
